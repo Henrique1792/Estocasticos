d7adf6843ddcfea13110532ee5687e62e10fafa2
Henrique1792 <freitash1792@gmail.com>
2019-06-15 10:36:10 -0300

    fix #1

--- Ex1/python/Ex1.py
+++ Ex1/python/Ex1.py
@@ -1,5 +1,6 @@
 import random as rd
 from math import exp
+from statistics import mean, median, mode, variance, stdev, StatisticsError
 
 
 def main():
@@ -18,14 +19,18 @@ def main():
     TK = []
     for k in range(10):
         TK.append(0)
-        for i in range(k):
-            TK[k] = TK[k] + vX[i]
+        if(k == 0):
+            TK[k] = vX[k]
+            TK[k] = TK[k]/87600
+        else:
+            for i in range(k):
+                TK[k] = TK[k] + vX[i]
+                TK[k] = TK[k]/87600
 
     print("TK: ", TK, "\n")
 
     # Passo 3: Calcular o valor atual dos custos de reposição
     # função  definida por lambda
-
     fnc = lambda x: 10*exp(-0.952380952381*x)
     CK = []
     for k in TK:
@@ -33,16 +38,15 @@ def main():
     print("CK: ", CK, "\n")
 
 # Medidas de posição
-#    try:
-#        print("Média: %f\nMediana: %f\nModa: %f \n" % (mean(xDensity),
-#                                                        median(xDensity),
-#                                                        mode(xDensity)))
-#    except StatisticsError:
-#        print("Média: %f\nMediana: %f\nSem Moda\n" % (mean(xDensity),
-#                                                       median(xDensity)))
+    try:
+        print("Média: %f\nMediana: %f\nModa: %f \n" % (mean(CK),
+                                                       median(CK),
+                                                       mode(CK)))
+    except StatisticsError:
+        print("Média: %f\nMediana: %f\nSem Moda\n" % (mean(CK),
+                                                      median(CK)))
 # Medidas de Dispersão
-#    print("Variância: %f\nDesvio Padrão: %f\n" % (variance(xDensity),
-    #                                                    stdev(xDensity)))
+    print("Variância: %f\nDesvio Padrão: %f\n" % (variance(CK), stdev(CK)))
 
 
 main()
