84240d0fa14ded81375610aabeec3c7e842c06b0
Henrique1792 <freitash1792@gmail.com>
2019-06-26 21:25:37 -0300

    export data for all exercises :sunglasses:

--- Ex1/python/Ex1.py
+++ Ex1/python/Ex1.py
@@ -1,7 +1,9 @@
-import numpy as np
+# import block
 import random as rd
+import json
+# method block
 from math import exp
-from bokeh.plotting import output_file, show
+from bokeh.io import export_png
 from statistics import mean, median, mode, variance, stdev, StatisticsError
 from genPlot import makeHistogram
 
@@ -9,7 +11,8 @@ from genPlot import makeHistogram
 def main():
     # Passo 1: gerar os tempos entre falhas
     rd.seed()
-
+    # json data
+    data = {}
     # vetor X gerado
     vX = []
 
@@ -24,41 +27,58 @@ def main():
         TK.append(0)
         if(k == 0):
             TK[k] = vX[k]
-            TK[k] = TK[k]/87600
         else:
             for i in range(k):
                 TK[k] = TK[k] + vX[i]
-                TK[k] = TK[k]/87600
+        TK[k] = TK[k] / 87600
 
     print("TK: ", TK, "\n")
 
     # Passo 3: Calcular o valor atual dos custos de reposição
     # função  definida por lambda
+    # 0.952380952381 obtido de: 1/(1+r), com r = 0.05
     fnc = lambda x: 10*exp(-0.952380952381*x)
     CK = []
     for k in TK:
         CK.append(fnc(k))
     print("CK: ", CK, "\n")
     meanVal = mean(CK)
+    # gen Json file for next exercises
 # Medidas de posição
     try:
         print("Média: %f\nMediana: %f\nModa: %f \n" % (meanVal,
                                                        median(CK),
                                                        mode(CK)))
+
+        data['X'] = vX
+        data['Tk'] = TK
+        data['cost'] = CK
+        data['mean'] = meanVal
+        data['variance'] = variance(CK)
+        data['mode'] = mode(CK)
+        data['stdev'] = stdev(CK)
     except StatisticsError:
         print("Média: %f\nMediana: %f\nSem Moda\n" % (meanVal,
                                                       median(CK)))
+        data['X'] = vX
+        data['Tk'] = TK
+        data['cost'] = CK
+        data['mean'] = meanVal
+        data['variance'] = variance(CK)
+        data['mode'] = 0
+        data['stdev'] = stdev(CK)
 # Medidas de Dispersão
     print("Variância: %f\nDesvio Padrão: %f\n" % (variance(CK), stdev(CK)))
 # Construção do gráfico
+    with open("../../assets/data/generated.json", "w") as wFile:
+        json.dump(data, wFile, indent=4)
     # Construir histograma da distribuição exponencial
 
 # Plot da Amostra
-    plt = makeHistogram("Custo", vX, CK)
-    # plt.circle(vX, CK, legend="Amostra", fill_color='red', size=8)
+    plt = makeHistogram("Relação de custo e tempo entre falhas", vX, CK)
+    plt.circle(vX, CK, legend="Amostra", fill_color='red', size=8)
 
-    output_file("plot.html")
-    show(plt)
+    export_png(plt, filename="../../assets/data/genPlot.png")
 #     plt = histogram(
 #        title="Tempo e custos",
 #        x_axis_label="Tempo",
--- Ex1/python/genPlot.py
+++ Ex1/python/genPlot.py
@@ -2,12 +2,14 @@ from bokeh.plotting import figure
 
 
 def makeHistogram(title, x, y):
-    p = figure(title=title, background_fill_color="#fafafa")
+    p = figure(title=title, tools='', background_fill_color="#fafafa")
     p.vbar(x=x, top=y, width=80)
     p.line(x, y, line_color="red", line_width=0.8, legend="Comportamento")
     p.legend.background_fill_color = "#fefefe"
     p.xaxis.axis_label = 'Time'
     p.yaxis.axis_label = 'Value'
+    p.x_range.start = 0
+    p.x_range.end = max(x)
     p.y_range.start = min(y)
     p.y_range.end = max(y) + 0.1
     p.grid.grid_line_color = "white"
